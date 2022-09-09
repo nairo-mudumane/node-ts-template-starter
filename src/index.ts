5d54cb15c91b5ee86b1d43d6d76e4aa7e529d8a3
nairo-mudumane <nairomudumane@outlook.com>
2022-09-09 11:17:53 +0200

    new template

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const PORT = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
